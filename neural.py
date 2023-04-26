a553c3b1dc89239385927c4558857ac46169a917
ozymandiaslone <saltsperkins@gmail.com>
2023-04-26 14:43:40 -0600

    ash

--- neural.py
+++ neural.py
@@ -30,7 +30,7 @@ async def process_stream(result_stream, msg):
     while True:
         try:
             result_chunk = next(result_stream)
-            data += ''.joing(result_chunk)
+            data += ''.join(result_chunk)
             await msg.edit(content=data)
         except StopIteration:
             break
